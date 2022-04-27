a3edc949c056be6c815c17f024702344431fe3d8
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-04-27 00:38:42 -0500

    project commit constants

---
+++ Trillium/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Trillium
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.CancelKeyPress += delegate (object sender, ConsoleCancelEventArgs e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Goodbye! ¡Adiós! Ciao! Adieu! Adeus! Tschüss! Пока! 再见 さようなら הֱיה שלום وداعا");
+            };
+
+            ClearLine();
+
+            var argList = args.ToList();
+            if(argList.Any())
+            {
+                argList.ForEach(x => {
+                    var arg = x.ToLower();
+                });
+            }
+        }
+
+        private static void ClearLine(string part1 = "", string part2 = "")
+        {
+            string spaces = new string(' ', part1.Length + part2.Length + 1);
+            Console.Write("\r{0}\r", spaces);
+        }
+
+        private static void SetCursor(string prompt, string line, int pos)
+        {
+            ClearLine(prompt, line);
+            Console.Write("{0}{1}\r{2}{3}",
+              prompt, line, prompt, line.Substring(0, pos));
+        }
+
+    }
+}
---
+++ Trillium/Utilities/TimeUtil.cs
@@ -0,0 +1,22 @@
+﻿namespace Trillium.Utilities
+{
+    internal class TimeUtil
+    {
+        public static long GetTime()
+        {
+            long epochTicks = new DateTime(1970, 1, 1).Ticks;
+            long nowTicks = DateTime.UtcNow.Ticks;
+            long timeStamp = ((nowTicks - epochTicks) / TimeSpan.TicksPerSecond);
+            return timeStamp;
+
+            //returns time in ticks from Epoch Time
+        }
+
+        public static DateTime ToDateTime(long unixTime)
+        {
+            DateTime frDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            frDateTime = frDateTime.AddSeconds(unixTime).ToLocalTime();
+            return frDateTime;
+        }
+    }
+}
