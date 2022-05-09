ac41aa4e365856083d0bef322e1f3875c947e6d8
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-05-08 23:17:39 -0500

    Xunit test project added with test

---
+++ Trillium.Test/Syntax/SyntaxFactTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trillium.Syntax;
+using Xunit;
+
+namespace Trillium.Test.Syntax
+{
+    public class SyntaxFactTests
+    {
+        [Theory]
+        [MemberData(nameof(GetSyntaxKindData))]
+        public void SyntaxFact_GetText_RoundTrips(SyntaxKind kind)
+        {
+            var text = SyntaxFacts.GetText(kind);
+            if (text == null)
+                return;
+
+            var tokens = SyntaxTree.ParseTokens(text);
+            var token = Assert.Single(tokens);
+            Assert.Equal(kind, token.Kind);
+            Assert.Equal(text, token.Text);
+        }
+
+        public static IEnumerable<object[]> GetSyntaxKindData()
+        {
+            var kinds = (SyntaxKind[])Enum.GetValues(typeof(SyntaxKind));
+            foreach (var kind in kinds)
+                yield return new object[] { kind };
+        }
+    }
+}
