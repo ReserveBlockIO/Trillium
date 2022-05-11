b82e71d97fe9f21ef65d79916351dcb5ad45ff30
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-05-11 16:20:38 -0500

    Trillium repl added outside main program

--- Trillium/Binding/BoundConditionalGotoStatement.cs
+++ Trillium/Binding/BoundConditionalGotoStatement.cs
@@ -4,16 +4,16 @@ namespace Trillium.Binding
 {
     internal sealed class BoundConditionalGotoStatement : BoundStatement
     {
-        public BoundConditionalGotoStatement(LabelSymbol label, BoundExpression condition, bool jumpIfFalse = false)
+        public BoundConditionalGotoStatement(LabelSymbol label, BoundExpression condition, bool jumpIfTrue = true)
         {
             Label = label;
             Condition = condition;
-            JumpIfFalse = jumpIfFalse;
+            JumpIfTrue = jumpIfTrue;
         }
 
         public override BoundNodeKind Kind => BoundNodeKind.ConditionalGotoStatement;
         public LabelSymbol Label { get; }
         public BoundExpression Condition { get; }
-        public bool JumpIfFalse { get; }
+        public bool JumpIfTrue { get; }
     }
 }
---
+++ Trillium/Binding/BoundVariableDeclaration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trillium.CodeAnalysis;
+
+namespace Trillium.Binding
+{
+    internal sealed class BoundVariableDeclaration : BoundStatement
+    {
+        public BoundVariableDeclaration(VariableSymbol variable, BoundExpression initializer)
+        {
+            Variable = variable;
+            Initializer = initializer;
+        }
+
+        public override BoundNodeKind Kind => BoundNodeKind.VariableDeclaration;
+        public VariableSymbol Variable { get; }
+        public BoundExpression Initializer { get; }
+    }
+}
