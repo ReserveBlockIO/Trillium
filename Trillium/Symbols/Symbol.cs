a096d5e0da955208919c37846ef46f1bb616c735
Mathis <20599614+mathis1337@users.noreply.github.com>
2022-05-11 23:12:19 -0500

    added abstract symbol class

---
+++ Trillium/Symbols/Symbol.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trillium.Symbols
+{
+    public abstract class Symbol
+    {
+        private protected Symbol(string name)
+        {
+            Name = name;
+        }
+
+        public abstract SymbolKind Kind { get; }
+        public string Name { get; }
+        public override string ToString() => Name;
+    }
+}
--- Trillium/Symbols/VariableSymbol.cs
+++ Trillium/Symbols/VariableSymbol.cs
@@ -1,19 +1,18 @@
 ﻿namespace Trillium.Symbols
 {
-    public sealed class VariableSymbol
+    public sealed class VariableSymbol : Symbol
     {
         internal VariableSymbol(string name, bool isReadOnly, Type type)
+            : base(name)
         {
-            Name = name;
             IsReadOnly = isReadOnly;
             Type = type;
         }
 
-        public string Name { get; }
+        public override SymbolKind Kind => SymbolKind.Variable;
         public bool IsReadOnly { get; }
         public Type Type { get; }
 
-        public override string ToString() => Name;
     }
 
 }
